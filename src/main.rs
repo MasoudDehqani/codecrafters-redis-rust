f2ea1e1d5008c72e5331435a848fa9a410c76735
Masoud <masoud67.md@gmail.com>
2025-10-27 07:10:01 +0330

    concurrent code refactored

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,7 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::{io::Read, net::TcpListener};
 
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    // net::TcpListener,
-};
-
-#[tokio::main]
-async fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
